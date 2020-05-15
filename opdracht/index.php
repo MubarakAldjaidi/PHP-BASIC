00c2d9e2b5b5867c36347d81f5efae6c17907133
MubarakAldjaidi <mubarak_aldjaidi@hotmail.com>
2020-05-15 05:54:31 +0200

    PHPBASICtaak01

--- opdracht/index.php
+++ opdracht/index.php
@@ -1,21 +1,19 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
+  <body>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Title</title>
   </head>
-  <?php
-  echo "Opdracht 1";
-  ?>
     <form action="index.php" method="get"><!-- dit laten staan! -->
-
     <p>Op deze website kun je checken welk datatype je hebt ingevoerd.</p>
     <P>Vul hieronder een getal, een decimaal, een tekst of de tekst true/false in en klik op check!</p> 
     <input type="text" name="input"><!-- dit laten staan! -->
     <button type="submit" name="submit">Check</button><!-- dit laten staan! -->
     <p>Het datatype van de waarde: <?php getInput(); ?> is </p>
+    <?php
+    $score = 0; ?>
 
     </form><!-- dit laten staan! -->
   </body>
