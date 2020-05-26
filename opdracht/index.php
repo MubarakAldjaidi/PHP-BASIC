1ef4d1bcccc543e5f5601c788e0ca63778de48fa
MubarakAldjaidi <mubarak_aldjaidi@hotmail.com>
2020-05-26 12:06:13 +0200

    Conditional Taak01

--- opdracht/index.php
+++ opdracht/index.php
@@ -3,11 +3,45 @@
 <body>
 
 <?php
-$score = 10;  
-echo ++$score;
+    $test1 = 10;
+    $test2 = "gewoon tekst";
 
-$score2 = 15
-echo --$score2;
+    var_dump($test1 == $test2);
+
+    $test3 = 10;
+    $test4 = "gewoon tekst";
+
+    var_dump($test3 === $test4);
+
+    $test5 = 10;
+    $test6 = "gewoon tekst";
+
+    var_dump($test5 != $test6);
+
+    $test7 = 10;
+    $test8 = "gewoon tekst";
+
+    var_dump($test7 > $test8);
+
+    $test9 = 10;
+    $test10 = "gewoon tekst";
+
+    var_dump($test9 < $test10);
+
+    $test11 = 10;
+    $test12 = "gewoon tekst";
+
+    var_dump($test11 <> $test12);
+
+    $test13 = 10;
+    $test14 = "gewoon tekst";
+
+    var_dump($test13 >= $test14);
+
+    $test15 = 10;
+    $test16 = "gewoon tekst";
+
+    var_dump($test15 <= $test16);
 ?>  
 
 </body>
