1d05d4d22d2b8d21cb2719191d80acef3d11cf89
MubarakAldjaidi <mubarak_aldjaidi@hotmail.com>
2020-05-26 11:39:46 +0200

    Arithmetics Taak03

--- opdracht/index.php
+++ opdracht/index.php
@@ -3,25 +3,11 @@
 <body>
 
 <?php
-  $a = 20;  
-  $a += 100;
+$score = 10;  
+echo ++$score;
 
-  echo $a;
-
-  $b = 40;  
-  $b -= 80;
-
-  echo $b;
-
-  $c = 30;  
-  $c *= 70;
-
-  echo $c;
-
-  $d = 20;  
-  $d /= 50;
-
-  echo $d;
+$score2 = 15
+echo --$score2;
 ?>  
 
 </body>
