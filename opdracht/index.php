455512436c8369c918bd4653fbea65ad6547845e
MubarakAldjaidi <mubarak_aldjaidi@hotmail.com>
2020-05-26 11:33:53 +0200

    Arithmetics Taak02

--- opdracht/index.php
+++ opdracht/index.php
@@ -1,22 +1,28 @@
-<!doctype html>
-    <head>
-    </head>
-    <body>
-      <?php
-     $getal1 = 5;
-     $getal2 = 10;
-     echo $getal1 + $getal2;
+<!DOCTYPE html>
+<html>
+<body>
 
-     $getal3 = 5;
-     $getal4 = 3;
-     echo $getal3 - $getal4;
+<?php
+  $a = 20;  
+  $a += 100;
 
-     $getal5 = 4;
-     $getal6 = 4;
-     echo $getal5 * $getal6;
+  echo $a;
 
-     $getal7 = 50;
-     $getal8 = 2;
-     echo $getal1 / $getal2;
-    </body>
-    </html>
+  $b = 40;  
+  $b -= 80;
+
+  echo $b;
+
+  $c = 30;  
+  $c *= 70;
+
+  echo $c;
+
+  $d = 20;  
+  $d /= 50;
+
+  echo $d;
+?>  
+
+</body>
+</html>
