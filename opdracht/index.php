0b0231904ba0d234dc2aa6ba1cdd1f6bf47e29c2
MubarakAldjaidi <mubarak_aldjaidi@hotmail.com>
2020-05-26 10:58:27 +0200

    Taak01

--- opdracht/index.php
+++ opdracht/index.php
@@ -1,20 +1,11 @@
 <!doctype html>
-<html lang="en">
-  <body>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Title</title>
-  </head>
-    <form action="index.php" method="get"><!-- dit laten staan! -->
-    <p>Op deze website kun je checken welk datatype je hebt ingevoerd.</p>
-    <P>Vul hieronder een getal, een decimaal, een tekst of de tekst true/false in en klik op check!</p> 
-    <input type="text" name="input"><!-- dit laten staan! -->
-    <button type="submit" name="submit">Check</button><!-- dit laten staan! -->
-    <p>Het datatype van de waarde: <?php getInput(); ?> is </p>
-    <?php
-    $score = 0; ?>
+    <head>
+    </head>
+    <body>
+      <?php
+         $score = 0;
 
-    </form><!-- dit laten staan! -->
-  </body>
-</html>
+         echo $score
+    ?>
+    </body>
+    </html>
