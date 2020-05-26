581a6a64b64483d605fd55116db8d70bd9c0ae86
MubarakAldjaidi <mubarak_aldjaidi@hotmail.com>
2020-05-26 11:05:36 +0200

    Taak07

--- opdracht/index.php
+++ opdracht/index.php
@@ -3,21 +3,10 @@
     </head>
     <body>
       <?php
-        $getal = 38;
-        echo gettype($getal) . "<br>";
-        
-        $cijfer = 3.2;
-        echo gettype($cijfer) . "<br>";
-        
-        $tekst = "Hello";
-        echo gettype($tekst) . "<br>";
-        
-        $vergelijking = true;
-        echo gettype($vergelijking ) . "<br>";
-        
-        $niets = null;
-        echo gettype($niets) . "<br>";
-        
+
+         $voornaam = "Mubarak";
+         $leeftijd = 22;
+         echo "Mijn naam is $voornaam en je bent $leeftijd jaar oud.";
     ?>
     </body>
     </html>
