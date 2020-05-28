e022392da7b311e4c1bada0cab64e2d6607da6f8
MubarakAldjaidi <mubarak_aldjaidi@hotmail.com>
2020-05-28 13:08:22 +0200

    Conditional Taak06

--- opdracht/index.php
+++ opdracht/index.php
@@ -3,24 +3,35 @@
 <body>
 
 <?php
-    $score = 10;
-    
-    switch($score) {
-        case 10:
-            echo "Wow wat een fantastische score!";
-            break;
-        case 9:
-            echo "Wow bijna alle punten nog 1 te gaan!";
-            break;
-        case 8:
-            echo "Goed bezig, bijna alle punten!";
-            break;
-        case 7:
-            echo "Ga zo door!";
-            break;
-            default:
-            echo "Nog even oefenen dan haal je vast alle punten!";
+    $var1 = 3;
+    $var2 = 10;
+    $var3 = 4;
+    $var4 = 11;
+
+    if($var1 < 10 && $var1 > 5) {
+        echo "het getal zit tussen de 10 en de 5!";
+    } else {
+        echo "het getal is groter dan 10 of kleiner dan 5!";
     }
+
+    if($var2 < 20 || $var2 > 10) {
+        echo "het getal zit tussen de 10 en de 20!";
+    } else {
+        echo "het getal is groter dan 10 of kleiner dan 20!";
+    }
+
+    if($var3 < 10 xor $var3 > 5) {
+        echo "het getal zit tussen de 10 en de 5!";
+    } else {
+        echo "het getal is groter dan 5 of kleiner dan 10!";
+    }
+
+    if($var1 < 20 ! $var1 > 10) {
+        echo "het getal zit tussen de 10 en de 20!";
+    } else {
+        echo "het getal is groter dan 10 of kleiner dan 20!";
+    }
+    
 ?>  
 
 </body>
